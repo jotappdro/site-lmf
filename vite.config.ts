95e13aae3f11d959cd7b8c011cbbc7b44f9bdc32
jotappdro <joaopsouza512@outlook.com>
2025-08-14 17:14:02 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: "/site-lmf/",
+    base: "/",
     server: {
       host: "::",
       port: 8080, 
