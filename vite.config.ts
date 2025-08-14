e8aa1baa7cadb0359aa0b335ef7173f4f6a8b6e4
jotappdro <joaopsouza512@outlook.com>
2025-08-14 13:24:34 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import fallback from "vite-plugin-pages-fallback"; // novo plugin
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,8 +13,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
+    fallback() // cria automaticamente um 404.html no build
   ].filter(Boolean),
   resolve: {
     alias: {
