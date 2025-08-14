8c888a6fc38b4fe5001875f27263e21f0082dc37
jotappdro <joaopsouza512@outlook.com>
2025-08-14 01:09:53 -0300

    Update SMF.tsx

--- src/pages/SMF.tsx
+++ src/pages/SMF.tsx
@@ -74,7 +74,7 @@ const SMF = () => {
               {/* Patrocinadores */}
               <div className="mb-20">
                 <h3 className="text-2xl font-semibold text-financial-primary mb-8">Patrocinadores</h3>
-                <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-12">
                   <div className="flex items-center justify-center h-24">
                     <img 
                       src={`${import.meta.env.BASE_URL}lovable-uploads/icatu2.png`}
