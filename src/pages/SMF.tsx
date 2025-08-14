3849ffdb8aff8709e8c28607cdfc1c1f1efb7122
jotappdro <joaopsouza512@outlook.com>
2025-08-14 01:05:35 -0300

    Update SMF.tsx

--- src/pages/SMF.tsx
+++ src/pages/SMF.tsx
@@ -72,7 +72,7 @@ const SMF = () => {
               </div>
 
               {/* Patrocinadores */}
-              c<div className="mb-20">
+              <div className="mb-20">
                 <h3 className="text-2xl font-semibold text-financial-primary mb-8">Patrocinadores</h3>
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                   <div className="flex items-center justify-center h-24">
@@ -80,7 +80,7 @@ const SMF = () => {
                       src={`${import.meta.env.BASE_URL}lovable-uploads/icatu2.png`}
                       alt="Icatu Vanguarda"
                       className="object-contain"
-                      style={{ height: "50px", width: "auto" }}
+                      style={{ height: "70px", width: "auto" }}
                     />
                   </div>
                   <div className="flex items-center justify-center h-24">
@@ -88,7 +88,7 @@ const SMF = () => {
                       src={`${import.meta.env.BASE_URL}lovable-uploads/fbb57043-4d30-470d-8971-bc6e5cffb5b1.png`}
                       alt="Sharp Capital"
                       className="object-contain"
-                      style={{ height: "50px", width: "auto" }}
+                      style={{ height: "70px", width: "auto" }}
                     />
                   </div>
                   <div className="flex items-center justify-center h-24">
@@ -96,7 +96,7 @@ const SMF = () => {
                       src={`${import.meta.env.BASE_URL}lovable-uploads/44bc199c-a18d-4d2e-9a57-1880d16425e9.png`}
                       alt="TRUXT"
                       className="object-contain"
-                      style={{ height: "50px", width: "auto" }}
+                      style={{ height: "70px", width: "auto" }}
                     />
                   </div>
                   <div className="flex items-center justify-center h-24">
@@ -104,7 +104,7 @@ const SMF = () => {
                       src={`${import.meta.env.BASE_URL}lovable-uploads/1f41757c-add6-49ae-9dd8-1c1a211dcdf9.png`}
                       alt="Vinci Compass"
                       className="object-contain"
-                      style={{ height: "50px", width: "auto" }}
+                      style={{ height: "70px", width: "auto" }}
                     />
                   </div>
                 </div>
