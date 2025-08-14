15698b18144f6ad6caf8b3b5e4f203ffaffddaf2
jotappdro <joaopsouza512@outlook.com>
2025-08-14 11:44:15 -0300

    Update SMF.tsx

--- src/pages/SMF.tsx
+++ src/pages/SMF.tsx
@@ -72,7 +72,7 @@ const SMF = () => {
               </div>
 
               {/* Patrocinadores */}
-              <div className="flex flex-col items-center" style={{ height: "300px" }}>">
+              <div className="flex flex-col items-center" style={{ height: "300px" }}>
                 <h3 className="text-2xl font-semibold text-financial-primary mb-8">Patrocinadores</h3>
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-16 justify-center">
                   <div className="flex-grow flex items-center">
