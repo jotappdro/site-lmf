6043700a4a08e53a2dc4e728872b1c88e24d759b
jotappdro <joaopsouza512@outlook.com>
2025-08-17 21:36:38 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,11 +20,15 @@ const Hero = () => {
           {/* Alinhamento de texto e padding ajustados */}
           <div className="max-w-md text-center lg:text-right">
             {/* Tamanho da fonte ajustado */}
-            <p className=" absolute -top-4 left-4   /* mobile: canto superior esquerdo */
-              lg:static lg:mx-auto lg:text-center  /* no desktop: centralizado */
+            <p 
+              className="
+              absolute top-4 left-4
+              lg:static lg:mx-auto lg:text-center
+              z-20
               text-white text-xl lg:text-2xl font-bold leading-relaxed drop-shadow-lg
-              ">
-              "There is no elevator to success, you have to take the stairs"
+              "
+              >
+              There is no elevator to success, you have to take the stairs
             </p>
 
           </div>
