be5e259cbe969bc7ab24a0c36408e9d2f2e90364
jotappdro <joaopsouza512@outlook.com>
2026-03-19 23:24:19 -0300

    Update Artigos.tsx

--- src/pages/Artigos.tsx
+++ src/pages/Artigos.tsx
@@ -68,7 +68,7 @@ const artigos = [
   autor: "Felipe Elgarten",
   data: "2023-07-10",
   capa: "/lovable-uploads/468e86_53232a69611d416ca18905cd85d76aeb~mv2.jpg",
-  pdfUrl: "/pdfs/Wal-Mart_ A extraordinariedade do simples.pdf"
+  pdfUrl: "public/pdfs/Wal-Mart_ A extraordinariedade do simples.pdf"
 }
   
 ];
