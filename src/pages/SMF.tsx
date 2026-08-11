edfeb5b6ec2030f7d6ad4ea9d362bc74752b3fd7
jotappdro <psvartman@gmail.com>
2026-08-10 22:59:08 -0300

    Update SMF.tsx

--- src/pages/SMF.tsx
+++ src/pages/SMF.tsx
@@ -145,7 +145,7 @@ const SMF = () => {
                     <div className="space-y-4">
                       <div className="flex flex-col md:flex-row md:items-center items-start gap-4 md:gap-6 p-6 bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow border border-gray-100">
                         <div className="text-lg font-medium text-primary mb-2 md:mb-0 md:min-w-[60px] text-center">
-                          11h30
+                          11h
                           <div className="block md:hidden h-0.5 bg-primary mt-1 w-full rounded-full"></div>
                         </div>
                         <div className="hidden md:block w-1 bg-primary rounded-full self-stretch"></div>
@@ -157,6 +157,7 @@ const SMF = () => {
                         </div>
                         </div>
                       </div>
+                  </div>
 
                       <div className="flex flex-col md:flex-row md:items-center items-start gap-4 md:gap-6 p-6 bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow border border-gray-100">
                         <div className="text-lg font-medium text-primary mb-2 md:mb-0 md:min-w-[60px] text-center">
